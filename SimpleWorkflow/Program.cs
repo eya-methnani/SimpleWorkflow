b12c05e1270fd67d04ae7ddffcfb88e8ed2ab5d1
eya-methnani <eya.methnani@ensi-uma.tn>
2024-12-23 19:23:09 +0100

    Add project files.

---
+++ SimpleWorkflow/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Activities;
+using System.Activities.Statements;
+using System.Linq;
+
+namespace SimpleWorkflow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Activity workflow1 = new Workflow1();
+            WorkflowInvoker.Invoke(workflow1);
+            Console.ReadLine();
+        }
+    }
+}
